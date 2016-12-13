f86f26de692533c5ce3a2973ce52135cd2b6c823
Windsor Kwan-Chun Ting <windsor-kwan-chun.ting.1@ulaval.ca>
2016-12-13 13:51:26 -0500

    Bugfix to TDT_normalization
    Bugfix for the above script:
    -The mean was previously calculated only for the first row, and that
    value used for the normalization.
    -This has now been corrected to the intended action, which is to
    calculate the mean of each column, the corresponding values which are
    subtracted from the mean_rect_EMGs for normalization.
    -As a result the graphs obtained from EMG_plot make more sense with
    post-normalization values deviating from zero and not another constant.

--- TDT_normalization.m
+++ TDT_normalization.m
@@ -7,7 +7,7 @@
 function [ mean_norm_rect_EMGs ] = TDT_normalization ( mean_rect_EMGs, zerobound, time_axis )
     % calculate the mean of the baseline data, between the start of data
     % collection and the zerobound (t = 0 relative to stim)
-    baselinemean = mean(mean_rect_EMGs(1:zerobound));
+    baselinemean = mean(mean_rect_EMGs([1 zerobound],:));
     % assign 'mean_norm_rect_EMGs' as the array containing these normalized values
     % (each value in 'mean_rect_EMGs' is subtracted from the baseline mean)
     mean_norm_rect_EMGs = bsxfun(@minus, mean_rect_EMGs, baselinemean);
